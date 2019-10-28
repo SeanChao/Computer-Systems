2680a70bd6e3ae8b9f7f83671209b65dfcb34e7e
SeanChao <seanchao0804@gmail.com>
2019-10-28 13:39:42 +0800

    update datalab-ics

--- 3-machine-level-rep-of-programs/data-movement/mov.c
+++ 3-machine-level-rep-of-programs/data-movement/mov.c
@@ -1,3 +1,65 @@
+// char short int long
+
+// typedef char src_t;
+// typedef char dst_t;
+//	movzbl	(%rdi), %eax
+//	movb	%al, (%rsi)
+
+// typedef char src_t;
+// typedef short dst_t;
+// movsbw	(%rdi), %ax
+// movw	%ax, (%rsi)
+
+// typedef char src_t;
+// typedef int dst_t;
+//	movsbl	(%rdi), %eax
+//	movl	%eax, (%rsi)
+
+// typedef char src_t;
+// typedef long dst_t;
+//	movsbq	(%rdi), %rax
+//	movq	%rax, (%rsi)
+
+// typedef char src_t;
+// typedef unsigned char dst_t;
+// movzbl	(%rdi), %eax
+// movb	%al, (%rsi)
+
+// typedef char src_t;
+// typedef unsigned short dst_t;
+// movsbw	(%rdi), %ax
+// movw	%ax, (%rsi)
+
+// typedef char src_t;
+// typedef unsigned int dst_t;
+// movsbl	(%rdi), %eax
+// movl	%eax, (%rsi)
+
+// typedef char src_t;
+// typedef unsigned long dst_t;
+// movsbq	(%rdi), %rax
+// movq	%rax, (%rsi)
+
+// typedef int src_t;
+// typedef char dst_t;
+//	movl	(%rdi), %eax
+//	movb	%al, (%rsi)
+
+// typedef int src_t;
+// typedef int dst_t;
+//  movl	(%rdi), %eax
+//  movl	%eax, (%rsi)
+
+// typedef int src_t;
+// typedef short dst_t;
+//  movl	(%rdi), %eax
+//  movw	%ax, (%rsi)
+
+// typedef int src_t;
+// typedef long dst_t;
+//	movslq	(%rdi), %rax
+//	movq	%rax, (%rsi)
+
 // typedef long src_t;
 // typedef long dst_t;
 // 	movq	(%rdi), %rax
@@ -13,14 +75,28 @@
 //  movsbl	(%rdi), %eax
 //  movl	%eax, (%rsi)
 
+// unsigned short -> (signed)
+// typedef unsigned short src_t;
+// typedef char dst_t;
+//	movzwl	(%rdi), %eax
+//	movb	%al, (%rsi)
+// typedef unsigned short src_t;
+// typedef short dst_t;
+//	movzwl	(%rdi), %eax
+//	movw	%ax, (%rsi)
+// typedef unsigned short src_t;
+// typedef int dst_t;
+// typedef unsigned short src_t;
+// typedef long dst_t;
+
 // typedef unsigned src_t;
 // typedef long dst_t;
 //	movl	(%rdi), %eax
 //  movq	%rax, (%rsi)
 
-// typedef int src_t;
-// typedef char dst_t;
-//	movl	(%rdi), %eax
+// typedef unsigned char src_t;
+// typedef unsigned char dst_t;
+//	movzbl	(%rdi), %eax
 //	movb	%al, (%rsi)
 
 // typedef unsigned char src_t;
@@ -28,12 +104,35 @@
 //	movzbl	(%rdi), %eax
 //	movl	%eax, (%rsi)
 
-typedef char src_t;
-typedef short dst_t;
+// typedef unsigned char src_t;
+// typedef char dst_t;
+//	movzbl	(%rdi), %eax
+//	movb	%al, (%rsi)
+
+// typedef unsigned char src_t;
+// typedef short dst_t;
+//	movzbl	(%rdi), %eax
+//	movw	%ax, (%rsi)
+
+// typedef unsigned char src_t;
+// typedef int dst_t;
+//	movzbl	(%rdi), %eax
+//	movl	%eax, (%rsi)
+
+// typedef unsigned char src_t;
+// typedef long dst_t;
+//	movzbl	(%rdi), %eax
+//	movq	%rax, (%rsi)
+
+// typedef char src_t;
+// typedef short dst_t;
 //	movsbw	(%rdi), %ax
 //	movw	%ax, (%rsi)
 
-dst_t test(src_t *sp, dst_t *dp) {
+// typedef unsigned long src_t;
+// typedef short dst_t;
+
+dst_t cast(src_t *sp, dst_t *dp) {
     *dp = (dst_t)*sp;
     return *dp;
 }
--- 3-machine-level-rep-of-programs/data-movement/mov.s
+++ 3-machine-level-rep-of-programs/data-movement/mov.s
@@ -1,15 +1,15 @@
 	.file	"mov.c"
 	.text
-	.globl	test
-	.type	test, @function
-test:
+	.globl	cast
+	.type	cast, @function
+cast:
 .LFB0:
 	.cfi_startproc
-	movsbw	(%rdi), %ax
-	movw	%ax, (%rsi)
+	movzbl	(%rdi), %eax
+	movb	%al, (%rsi)
 	ret
 	.cfi_endproc
 .LFE0:
-	.size	test, .-test
+	.size	cast, .-cast
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
